385085163b7628a95255fcb6b4570804363a91bc
mark0 <59284400+mark0-cn@users.noreply.github.com>
2023-08-30 20:41:56 +0800

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,13 @@
 use std::fs;
 
-static GOCKER_HOME_PATH: &str = "/var/lib/gocker";
-static GOCKER_TEMP_PATH: &str = "/var/lib/gocker/tmp";
-static GOCKER_IMAGES_PATH: &str = "/var/lib/gocker/images";
-static GOCKER_CONTAINERS_PATH: &str = "/var/run/gocker/containers";
-static GOCKER_NET_NS_PATH: &str = "/var/run/gocker/net-ns";
+static RSDOCKER_HOME_PATH: &str = "/var/lib/rsdocker/";
+static RSDOCKER_TEMP_PATH: &str = "/var/lib/rsdocker/tmp/";
+static RSDOCKER_IMAGES_PATH: &str = "/var/lib/rsdocker/images/";
+static RSDOCKER_CONTAINERS_PATH: &str = "/var/run/rsdocker/containers/";
+static RSDOCKER_NET_NS_PATH: &str = "/var/run/rsdocker/net-ns/";
 
 pub fn init_rsdocker_dirs() -> Result<(), std::io::Error> {
-    let dirs = [GOCKER_HOME_PATH, GOCKER_TEMP_PATH, GOCKER_IMAGES_PATH, GOCKER_CONTAINERS_PATH, GOCKER_NET_NS_PATH];
+    let dirs = [RSDOCKER_HOME_PATH, RSDOCKER_TEMP_PATH, RSDOCKER_IMAGES_PATH, RSDOCKER_CONTAINERS_PATH, RSDOCKER_NET_NS_PATH];
 
     for dir in dirs.iter() {
         if !fs::metadata(&dir).is_ok() {
@@ -18,9 +18,9 @@ pub fn init_rsdocker_dirs() -> Result<(), std::io::Error> {
 }
 
 pub fn get_rsdocker_images_path() -> String {
-    GOCKER_IMAGES_PATH.to_string()
+    RSDOCKER_IMAGES_PATH.to_string()
 }
 
 pub fn get_rsdocker_tmp_path() -> String {
-    GOCKER_TEMP_PATH.to_string()
+    RSDOCKER_TEMP_PATH.to_string()
 }
